7c37632acf741fdc3a51db1c749d4c917589fa4c
Krisztián  Gödrei <godreikrisztian@gmail.com>
2018-04-15 16:25:14 +0200

    do not use releasean

--- main.go
+++ main.go
@@ -1,51 +1,101 @@
 package main
 
 import (
+	"fmt"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
 
-	"github.com/bitrise-io/go-utils/command"
 	"github.com/bitrise-io/go-utils/fileutil"
 	"github.com/bitrise-io/go-utils/log"
-	"github.com/bitrise-io/go-utils/pathutil"
 	"github.com/bitrise-tools/go-steputils/tools"
+	"github.com/godrei/steps-generate-changelog/git"
+	version "github.com/hashicorp/go-version"
 )
-
-const defaultReleaseConfig = `release:
-  development_branch: master
-changelog:
-  path: CHANGELOG.md
-  content_template: |-
-    {{range .ContentItems}}### {{.EndTaggedCommit.Tag}} ({{.EndTaggedCommit.Date.Format "2006 Jan 02"}})
-
-    {{range .Commits}}* [{{firstChars .Hash 7}}] {{.Message}}
-    {{end}}
-    {{end}}
-  header_template: '## Changelog (Current version: {{.Version}})'
-  footer_template: 'Updated: {{.CurrentDate.Format "2006 Jan 02"}}'`
-
-func installedInPath(name string) bool {
-	cmd := exec.Command("which", name)
-	outBytes, err := cmd.Output()
-	return err == nil && strings.TrimSpace(string(outBytes)) != ""
-}
 
 func failf(format string, args ...interface{}) {
 	log.Errorf(format, args...)
 	os.Exit(1)
 }
 
+func releaseCommits(newVersion string) ([]git.Commit, error) {
+	ver, err := version.NewVersion(newVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	dir, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	firstCommit, err := git.FirstCommit(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	lastCommit, err := git.LastCommit(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	taggedCommits, err := git.VersionTaggedCommits(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	startCommit := firstCommit
+	includeFirst := true
+	endCommit := lastCommit
+	if len(taggedCommits) > 0 {
+		lastTaggedCommit := taggedCommits[len(taggedCommits)-1]
+		lastVersion, err := version.NewVersion(lastTaggedCommit.Tag)
+		if err != nil {
+			return nil, err
+		}
+
+		if ver.LessThan(lastVersion) {
+			// collecting previous changelogs
+			return nil, fmt.Errorf("new version (%s) is less then the most recent tag (%s)", ver, lastTaggedCommit.Tag)
+		} else if ver.GreaterThan(lastVersion) {
+			// collecting changelog for a new version
+			startCommit = taggedCommits[len(taggedCommits)-1]
+			includeFirst = false
+		} else if len(taggedCommits) > 1 {
+			// collecting changelog between existing versions
+			startCommit = taggedCommits[len(taggedCommits)-2]
+			endCommit = taggedCommits[len(taggedCommits)-1]
+			includeFirst = false
+		}
+	}
+
+	commits, err := git.Commits(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	var releaseCommits []git.Commit
+	for _, commit := range commits {
+		if commit.Date.Before(startCommit.Date) {
+			continue
+		}
+		if !includeFirst && commit.Date.Equal(startCommit.Date) {
+			continue
+		}
+		if commit.Date.After(endCommit.Date) {
+			break
+		}
+		releaseCommits = append(releaseCommits, commit)
+	}
+
+	return releaseCommits, nil
+}
+
 func main() {
 	version := os.Getenv("new_version")
 	changelogPth := os.Getenv("changelog_pth")
-	releaseConfigContent := os.Getenv("release_config")
 
 	log.Infof("Configs:")
 	log.Printf("new_version: %s", version)
 	log.Printf("changelog_pth: %s", changelogPth)
-	log.Printf("release_config:\n%s", releaseConfigContent)
 
 	if version == "" {
 		failf("Next version not defined")
@@ -55,60 +105,18 @@ func main() {
 		failf("Changelog path not defined")
 	}
 
-	if releaseConfigContent == "" {
-		releaseConfigContent = defaultReleaseConfig
-	}
-
-	if !installedInPath("releaseman") {
-		cmd := command.New("go", "get", "-u", "github.com/bitrise-tools/releaseman")
-
-		log.Infof("\nInstalling releaseman")
-		log.Donef("$ %s", cmd.PrintableCommandArgs())
-
-		if out, err := cmd.RunAndReturnTrimmedCombinedOutput(); err != nil {
-			failf("Failed to install releaseman: %s", out)
-		}
-	}
-
-	pwd, err := os.Getwd()
+	commits, err := releaseCommits(version)
 	if err != nil {
-		failf("Failed to get working directory")
+		panic(err)
 	}
 
-	releaseConfigPth := filepath.Join(pwd, "release_config.yml")
-	if exist, err := pathutil.IsPathExists(releaseConfigPth); err != nil {
-		failf("Failed to check if release config file exist: %s", err)
-	} else if !exist {
-		if err := fileutil.WriteStringToFile(releaseConfigPth, releaseConfigContent); err != nil {
-			failf("Failed to create release config file: %s", err)
-		}
-	}
-
-	// sandbox
-	if err := command.New("git", "fetch", "--all", "--tags", "--prune").Run(); err != nil {
-		failf("Failed to fetch tags: %s", err)
-	}
-
-	if err := command.New("git", "checkout", "master").Run(); err != nil {
-		failf("Failed to checkout tag: %s", err)
-	}
-	//
-
-	log.Infof("\nGenerating changelog...")
-
-	cmd := command.New("releaseman", "--ci", "create-changelog", "--version", version, "--changelog-path", changelogPth)
-
-	log.Printf("$ %s", cmd.PrintableCommandArgs())
-
-	if out, err := cmd.RunAndReturnTrimmedCombinedOutput(); err != nil {
-		log.Errorf("command failed:")
-		log.Printf(out)
-		os.Exit(1)
-	}
-
-	changelog, err := fileutil.ReadStringFromFile(changelogPth)
+	changelog, err := ChangelogContent(commits, version)
 	if err != nil {
-		failf("Failed to read changelog: %s", err)
+		panic(err)
+	}
+
+	if err := fileutil.WriteStringToFile(changelogPth, changelog); err != nil {
+
 	}
 
 	log.Infof("\nChangelog:")
